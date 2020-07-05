ea2ea78d96151ece69f4b3f8bff87d90740f0dc6
dat-adi <dat.adithya@gmail.com>
2020-07-05 14:09:31 +0530

    Assets and ticket conversion to json

--- src/main.py
+++ src/main.py
@@ -1,32 +1,12 @@
-from modules.ticket_header import ticket_maker
+import json
+from modules.ticket_header import selection
 
 if __name__ == "__main__":
 
     version_info = open("../assets/version.txt", 'r').read()
     print("Eisenhower Tickets | " + version_info)
-    print("Categories")
-    print("-"*10)
-
-    print("1. Do       - Urgent and Important")
-    print("2. Decide   - Not Urgent and Important")
-    print("3. Delegate - Urgent and Not Important")
-    print("4. Delete   - Not Urgent and Not Important\n")
-
-    categories = ["DO", "DEC", "DLG", "DEL"]
-    category_number = int(input("Enter the category number : "))
-    content = input("Enter the task at hand : ")
-    more_info = input("Enter details of task : ")
-    category = categories[category_number-1]
-
-    print("Ticket Created\n")
-
-    print("Ticket Details")
-    print("-"*45)
-    sample_ticket = ticket_maker(category, content, more_info)
-    print("Time stamp : {}".format(sample_ticket.timestamp))
-    print("Category   : {}".format(sample_ticket.category))
-    print("-"*45)
-    print("Content    : {}".format(sample_ticket.content))
-    print("-"*45)
-    print("More Info  : {}".format(sample_ticket.more_info))
-    print("-"*45)
+    ticket = selection()
+    print("Adding Ticket to Eisen's Tickets...")
+    print(ticket)
+    with open("../assets/eisen-tickets.txt", 'w') as f:
+        f.write(json.dumps(ticket, indent=4, sort_keys=True))
--- src/modules/ticket_header.py
+++ src/modules/ticket_header.py
@@ -15,25 +15,50 @@ Task :
 '''
 
 class ticket_maker:
-    def __init__(self, category, content, more_info):
+    def __init__(self, ticket_id, category, task, more_info):
+        self.ticket_id = ticket_id
         self.category = category
         self.timestamp = dt.now()
-        self.content = content
+        self.task = task
         self.more_info = more_info
-        self.ticket_info = {"Time Stamp" : self.timestamp, "Category" : self.category, "Content" : self.content, "More Info" : self.more_info}
+
+        self.ticket = {}
+
+        self.ticket[self.ticket_id] = {"Time Stamp" : str(self.timestamp), "Category" : self.category, "Content" : self.task, "More Info" : self.more_info}
 
     def change_category(self, category):
         self.category = category
 
-    def change_content(self, content):
-        self.content = content
+    def change_task(self, task):
+        self.task = task
 
     def add_more_info(self, more_info):
         self.more_info = more_info
 
     def return_ticket_info(self):
-        return self.ticket_info
+        return self.ticket
+
+def selection():
+    print("Categories")
+    print("-"*10)
+
+    print("1. Do       - Urgent and Important")
+    print("2. Decide   - Not Urgent and Important")
+    print("3. Delegate - Urgent and Not Important")
+    print("4. Delete   - Not Urgent and Not Important\n")
+
+    categories = ["DO", "DEC", "DLG", "DEL"]
+    category_number = int(input("Enter the category number : "))
+    task = input("Enter the task at hand : ")
+    more_info = input("Enter details of task : ")
+    category = categories[category_number-1]
+    ticket_id = 1 # still have to work on the sorting of the ticket ids
+    print("[INFO] Ticket Created...\n")
+
+    return ticket_maker(ticket_id, category, task, more_info).return_ticket_info()
+    
 
 if __name__ == "__main__":
-    sample_ticket = ticket_maker("test_category", "testing it out", "a little more to go on")
-    print("Time stamp : {}\nCategory : {}\nContent : {}\nMore Info : {}".format(sample_ticket.timestamp, sample_ticket.category, sample_ticket.content, sample_ticket.more_info))
+    sample_ticket = ticket_maker(23, "test_category", "testing it out", "a little more to go on")
+    print("Ticket ID : {}\nTime stamp : {}\nCategory : {}\nTask : {}\nMore Info : {}".format(sample_ticket.ticket_id,sample_ticket.timestamp, sample_ticket.category, sample_ticket.task, sample_ticket.more_info))
+    print(sample_ticket)
