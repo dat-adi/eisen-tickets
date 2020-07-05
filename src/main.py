88c8b5ae4549a0a20a475904f779fd09286d21c9
dat-adi <dat.adithya@gmail.com>
2020-07-05 12:32:41 +0530

    Added version, and more info issue resolved.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from modules.ticket_header import ticket_maker
 
 if __name__ == "__main__":
 
-    print("Eisenhower Tickets |")
+    version_info = open("../assets/version.txt", 'r').read()
+    print("Eisenhower Tickets | " + version_info)
     print("Categories")
     print("-"*10)
 
@@ -14,17 +15,18 @@ if __name__ == "__main__":
     categories = ["DO", "DEC", "DLG", "DEL"]
     category_number = int(input("Enter the category number : "))
     content = input("Enter the task at hand : ")
-
+    more_info = input("Enter details of task : ")
     category = categories[category_number-1]
 
     print("Ticket Created\n")
+
     print("Ticket Details")
     print("-"*45)
-    sample_ticket = ticket_maker(category, content)
+    sample_ticket = ticket_maker(category, content, more_info)
     print("Time stamp : {}".format(sample_ticket.timestamp))
     print("Category   : {}".format(sample_ticket.category))
     print("-"*45)
     print("Content    : {}".format(sample_ticket.content))
     print("-"*45)
-    print("More Info  : {}".format(sample_ticket.more_info())
+    print("More Info  : {}".format(sample_ticket.more_info))
     print("-"*45)
--- src/modules/ticket_header.py
+++ src/modules/ticket_header.py
@@ -15,10 +15,12 @@ Task :
 '''
 
 class ticket_maker:
-    def __init__(self, category, content):
+    def __init__(self, category, content, more_info):
         self.category = category
         self.timestamp = dt.now()
         self.content = content
+        self.more_info = more_info
+        self.ticket_info = {"Time Stamp" : self.timestamp, "Category" : self.category, "Content" : self.content, "More Info" : self.more_info}
 
     def change_category(self, category):
         self.category = category
@@ -29,6 +31,9 @@ class ticket_maker:
     def add_more_info(self, more_info):
         self.more_info = more_info
 
+    def return_ticket_info(self):
+        return self.ticket_info
+
 if __name__ == "__main__":
-    sample_ticket = ticket_maker("test_category", "testing it out")
-    print("Time stamp : {}\nCategory : {}\nContent : {}".format(sample_ticket.timestamp, sample_ticket.category, sample_ticket.content))
+    sample_ticket = ticket_maker("test_category", "testing it out", "a little more to go on")
+    print("Time stamp : {}\nCategory : {}\nContent : {}\nMore Info : {}".format(sample_ticket.timestamp, sample_ticket.category, sample_ticket.content, sample_ticket.more_info))
