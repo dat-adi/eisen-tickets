34407e31138c754f7568d080de8d762da49f3118
dat-adi <dat.adithya@gmail.com>
2020-07-10 20:55:27 +0530

    updated sql, going to try one table approach instead.

--- src/modules/test_sql.py
+++ src/modules/test_sql.py
@@ -1,34 +1,4 @@
-import sqlite3
-from sqlite3 import Error
-
-
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-
-    return conn
-
-
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
+from modules.db_insertion import create_table, insertion_row, create_connection
 
 
 def main():
@@ -36,29 +6,27 @@ def main():
 
     sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
                                         id integer PRIMARY KEY,
-                                        category text NOT NULL,
+                                        category text,
                                     ); """
 
     sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
                                     id integer PRIMARY KEY,
-                                    ticket_id integer NOT NULL,
                                     timestamp text NOT NULL,
                                     category text NOT NULL,
                                     task text NOT NULL,
+                                    project_id integer NOT NULL,
                                     more_info text,
                                     FOREIGN KEY (project_id) REFERENCES projects (id)
                                 );"""
 
-    # create a database connection
     conn = create_connection(database)
 
-    # create tables
     if conn is not None:
-        # create projects table
         create_table(conn, sql_create_projects_table)
 
-        # create tasks table
-        create_table(conn, sql_create_tasks_table)
+        project_id = create_table(conn, sql_create_tasks_table)
+        details = ('09/01/2020', "DO", "nothing", project_id, "doing absolutely nothing might lead to a higher level of understanding.")
+        insertion_row(conn, details)
     else:
         print("Error! cannot create the database connection.")
 
