d6b18143a06de68fabea910564011ce7846c58f9
dat-adi <dat.adithya@gmail.com>
2020-07-11 22:37:59 +0530

    integration with main.py complete

--- src/main.py
+++ src/main.py
@@ -4,5 +4,14 @@ if __name__ == "__main__":
 
     version_info = open("../assets/version.txt", 'r').read()
     print("Eisenhower Tickets | " + version_info)
-    print("Starting up the selection screen...")
-    table_initialization()
+
+    while True:
+        print("Starting up the selection screen...")
+        table_initialization()
+        cont = input("Do you wish to restart the service? (y/n) : ")
+        if cont is 'n':
+            print("Exiting Eisen's Tickets...")
+            break
+        print("Restarting Eisen's Tickets...")
+        print('-'*10)
+        print()
